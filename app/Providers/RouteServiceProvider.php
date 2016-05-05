f342b83d23b77c86bf750381a3da1e2fe27cd9dd
Alexander Vasiliev <alexander.vasiliev@postindustria.com>
2016-05-05 13:12:54 +0300

    added routes

---
+++ app/Providers/RouteServiceProvider.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Providers;
+
+use Config;
+use Illuminate\Routing\Router;
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    /**
+     * This namespace is applied to your controller routes.
+     *
+     * In addition, it is set as the URL generator's root namespace.
+     *
+     * @var string
+     */
+    protected $namespace = 'App\Http\Controllers';
+
+    /**
+     * Define your route model bindings, pattern filters, etc.
+     *
+     * @param  \Illuminate\Routing\Router  $router
+     * @return void
+     */
+    public function boot(Router $router)
+    {
+        //
+
+        parent::boot($router);
+    }
+
+    /**
+     * Define the routes for the application.
+     *
+     * @param  \Illuminate\Routing\Router  $router
+     * @return void
+     */
+    public function map(Router $router)
+    {
+        $hosts = Config::get('app.hosts');
+        foreach ($hosts as $hostKey => $hostName) {
+            if ($hostName == $_SERVER['HTTP_HOST']) {
+                $method = 'mapWebRoutes' . ucfirst($hostKey);
+                $this->{$method}($router);
+                break;
+            }
+        }
+    }
+
+    protected function mapWebRoutesUsa(Router $router)
+    {
+        $router->group([
+            'namespace' => $this->namespace, 'middleware' => 'web',
+        ], function ($router) {
+            require app_path('Http/routes_usa.php');
+        });
+    }
+
+    protected function mapWebRoutesUsa2(Router $router)
+    {
+        $router->group([
+            'namespace' => $this->namespace, 'middleware' => 'web',
+        ], function ($router) {
+            require app_path('Http/routes_usa2.php');
+        });
+    }
+
+    protected function mapWebRoutesUsa3(Router $router)
+    {
+        $router->group([
+            'namespace' => $this->namespace, 'middleware' => 'web',
+        ], function ($router) {
+            require app_path('Http/routes_usa3.php');
+        });
+    }
+}
